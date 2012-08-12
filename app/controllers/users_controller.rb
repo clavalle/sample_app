30ffc7580c018c2b5d53c2fc5b54d4832f9638c0
Craig LaValle <craig.lavalle@gmail.com>
2012-08-12 16:37:36 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_filter :signed_in_user,
+    only: [:index, :edit, :update, :destroy, :following, :followers]
   def show
     @user = User.find(params[:id])
   end
@@ -10,11 +11,35 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    if(@user.save)
+    if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
+  def update
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+  private
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
 end
